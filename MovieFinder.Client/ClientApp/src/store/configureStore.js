8f34a351d6bac2c00f1873482234781285cc6d6e
Lak <lakthinda@gmail.com>
2019-02-27 18:43:15 +1100

    Clean UI

--- MovieFinder.Client/ClientApp/src/store/configureStore.js
+++ MovieFinder.Client/ClientApp/src/store/configureStore.js
@@ -1,15 +1,11 @@
 ﻿import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import { routerReducer, routerMiddleware } from 'react-router-redux';
-import * as Counter from './Counter';
-import * as WeatherForecasts from './WeatherForecasts';
 import * as MovieStore from './MoviesStore';
 import * as MovieDetailsStore from './MovieDetailsStore';
 
 export default function configureStore(history, initialState) {
-  const reducers = {
-    counter: Counter.reducer,
-    weatherForecasts: WeatherForecasts.reducer,
+  const reducers = {    
     movies: MovieStore.reducer,
     movieDetails: MovieDetailsStore.reducer
   };
