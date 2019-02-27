494aedb58211e314ad0d8943ca30820d5fd0911a
Lak <lakthinda@gmail.com>
2019-02-28 09:05:34 +1100

    Clean up

--- MovieFinder.Client/Models/MovieDto.cs
+++ MovieFinder.Client/Models/MovieDto.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace MovieFinder.Client.Models
+﻿namespace MovieFinder.Client.Models
 {
     /// <summary>
     /// MovieDto - to pass into UI
